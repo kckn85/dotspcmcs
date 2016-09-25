f6460841410b7fb3a64f29805400952440a46ebe
kckn85 <kckn85@users.noreply.github.com>
2016-09-25 16:11:21 +0100

    Fix Emacs hanging due to tramp
    On branch master
    Changes to be committed:
    	modified:   init.el

--- init.el
+++ init.el
@@ -243,6 +243,8 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (setq tramp-ssh-controlmaster-options
+        "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
 )
 
 (defun dotspacemacs/user-config ()
@@ -252,9 +254,6 @@ layers configuration.
 This is the place where most of your configurations should be done. Unless it is
 explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
-
-  (setq tramp-ssh-controlmaster-options
-        "-o ControlMaster=auto -o ControlPath='tramp.%%C' -o ControlPersist=no")
 
   (setq ffap-machine-p-known 'reject)
 
